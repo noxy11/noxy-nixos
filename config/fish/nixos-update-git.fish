4e8cba5cd5a1aa59ab29f08b8d22a9b9eca22213
noxy11 <timofeyosn@gmail.com>
2026-09-17 14:20:03 +0700

    chore: auto-sync all configs and modules

--- config/fish/nixos-update-git.fish
+++ config/fish/nixos-update-git.fish
@@ -1,13 +1,12 @@
 function nixos-update-git --description 'Синхронизация NixOS и конфигов программ с GitHub'
     # 1. Создаем структуру папок
-    mkdir -p ~/.dotfiles/nixos
+    mkdir -p ~/.dotfiles/nixos/modules
     mkdir -p ~/.dotfiles/config
 
-    # 2. Копируем системные файлы NixOS и подпапку модулей внутрь nixos/
+    # 2. Копируем файлы конфигурации из /etc/nixos
     cp -u /etc/nixos/*.nix ~/.dotfiles/nixos/ 2>/dev/null
-    rsync -av --delete --exclude='.git' /etc/nixos/modules/ ~/.dotfiles/nixos/modules/ 2>/dev/null
 
-    # 3. Чисто и без ошибок копируем папки настроек программ
+    # 3. Копируем папки настроек программ
     rsync -av --delete --exclude='.git' ~/.config/nvim/ ~/.dotfiles/config/nvim/ 2>/dev/null
     rsync -av --delete ~/.config/fastfetch/ ~/.dotfiles/config/fastfetch/ 2>/dev/null
     rsync -av --delete ~/.config/kitty/ ~/.dotfiles/config/kitty/ 2>/dev/null
@@ -17,7 +16,7 @@ function nixos-update-git --description 'Синхронизация NixOS и к�
     cp -u ~/.config/fish/*.fish ~/.dotfiles/config/fish/ 2>/dev/null
     cp -u ~/.config/fish/functions/*.fish ~/.dotfiles/config/fish/ 2>/dev/null
 
-    # 5. Git-цепочка и сборка (с промежуточным индексированием файлов для Flakes)
+    # 5. Git-цепочка и сборка без dirty-предупреждений
     cd ~/.dotfiles
     git add -A
     git stash
