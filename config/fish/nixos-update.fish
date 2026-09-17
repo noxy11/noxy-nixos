af996bfb85f2999c4ba3bfbdcca252316cb3e0f5
noxy11 <timofeyosn@gmail.com>
2026-09-17 14:13:56 +0700

    chore: auto-sync all configs and modules

--- config/fish/nixos-update.fish
+++ config/fish/nixos-update.fish
@@ -1,3 +1,3 @@
 function nixos-update --description 'Просто применить систему из дотфайлов'
-    sudo nixos-rebuild switch --flake ~/.dotfiles#nixos $argv
+    sudo nixos-rebuild switch --flake ~/.dotfiles/nixos/#nixos $argv
 end
