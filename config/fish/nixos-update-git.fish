184d1cfc5efc9a3954ed54b0c005e9a248d2f028
noxy11 <timofeyosn@gmail.com>
2026-09-17 14:43:51 +0700

    chore: auto-sync all configs and modules

--- config/fish/nixos-update-git.fish
+++ config/fish/nixos-update-git.fish
@@ -1,18 +1,14 @@
 function nixos-update-git --description 'Полная синхронизация системы, программ и отправка на GitHub'
-    # Копируем ТОЛЬКО configuration.nix
     cp -u /etc/nixos/configuration.nix ~/.dotfiles/nixos/ 2>/dev/null
 
-    # Копируем настройки приложений
     rsync -av --delete --exclude='.git' ~/.config/nvim/ ~/.dotfiles/config/nvim/ 2>/dev/null
     rsync -av --delete ~/.config/fastfetch/ ~/.dotfiles/config/fastfetch/ 2>/dev/null
     rsync -av --delete --exclude='themes' ~/.config/kitty/ ~/.dotfiles/config/kitty/ 2>/dev/null
     
-    # Копируем функции Fish
     mkdir -p ~/.dotfiles/config/fish
     cp -u ~/.config/fish/*.fish ~/.dotfiles/config/fish/ 2>/dev/null
     cp -u ~/.config/fish/functions/*.fish ~/.dotfiles/config/fish/ 2>/dev/null
 
-    # Цепочка Git с умным слиянием (стратегия 'ours' отдает приоритет локальным конфигам)
     cd ~/.dotfiles
     git add -A
     git stash
@@ -21,7 +17,7 @@ function nixos-update-git --description 'Полная синхронизация
     git add -A
     nix flake update --flake ~/.dotfiles/nixos/ 2>/dev/null
     git add -A
-    sudo nixos-rebuild switch --flake "path:/home/noxy/.dotfiles/nixos/#nixos"
+    sudo nixos-rebuild switch --flake "path:/home/noxy/.dotfiles/nixos/#nixos" --impure
     git commit -m 'chore: auto-sync all configs and modules' --allow-empty
     git push origin main $argv
 end
--- config/fish/nixos-update.fish
+++ config/fish/nixos-update.fish
@@ -1,6 +1,4 @@
 function nixos-update --description 'Копирование конфигов из /etc/nixos в дотфайлы и быстрая локальная сборка'
-    # Копируем ТОЛЬКО configuration.nix, игнорируя hardware-configuration
     cp -u /etc/nixos/configuration.nix ~/.dotfiles/nixos/ 2>/dev/null
-    
-    sudo nixos-rebuild switch --flake "path:/home/noxy/.dotfiles/nixos/#nixos" $argv
+    sudo nixos-rebuild switch --flake "path:/home/noxy/.dotfiles/nixos/#nixos" --impure $argv
 end
